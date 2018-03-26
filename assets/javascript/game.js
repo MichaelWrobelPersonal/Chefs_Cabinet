e0dc6d4a8649ac238ec37b763ae46b685ea15ddb
Marton Kondray <mkondray@gmail.com>
2018-03-26 00:10:44 -0400

    Added an animated scroll to top button on each page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -173,4 +173,31 @@ $(document).ready(function() {
    }
 
 
+   //Scroll to top button
+    if ($('#back-to-top').length) {
+    var scrollTrigger = 100,
+        backToTop = function () {
+            var scrollTop = $(window).scrollTop();
+            if (scrollTop > scrollTrigger) {
+                $('#back-to-top').addClass('show');
+            } else {
+                $('#back-to-top').removeClass('show');
+            }
+        };
+
+    backToTop();
+    
+    $(window).on('scroll', function () {
+        backToTop();
+    });
+
+    $('#back-to-top').on('click', function (e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 700);
+      });
+    }
+
+
 });
